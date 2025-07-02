773bfbeb5efcf6c7e779060fa48ca5a0362f07f9
Valeriya Chulkova <valeriyastudy@mail.ru>
2025-07-02 12:24:33 +0500

    изменения для виртуалки

---
+++ wait_for_db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "db" -U "$POSTGRES_USER" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 2
+done
+
+>&2 echo "Postgres is up - continuing"
